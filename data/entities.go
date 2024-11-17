4a543c4c1b105da2ed5877c91a5f8b04e70d803c
obynonwane <biostechnologyng@gmail.com>
2024-11-17 05:52:49 +0100

    inventory category subcategory relations defined

--- data/entities.go
+++ data/entities.go
@@ -6,12 +6,58 @@ import (
 
 // User is the structure which holds one user from the database.
 type User struct {
-	ID        string    `json:"id"`
-	Email     string    `json:"email"`
-	FirstName string    `json:"first_name,omitempty"`
-	LastName  string    `json:"last_name,omitempty"`
-	Password  string    `json:"-"`
-	Verified  bool      `json:"verified"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID          string      `json:"id"`
+	Email       string      `json:"email"`
+	FirstName   string      `json:"first_name,omitempty"`
+	LastName    string      `json:"last_name,omitempty"`
+	Password    string      `json:"-"`
+	Verified    bool        `json:"verified"`
+	CreatedAt   time.Time   `json:"created_at"`
+	UpdatedAt   time.Time   `json:"updated_at"`
+	Inventories []Inventory `json:"inventories,omitempty"` // One-to-many relationship
+}
+
+type Category struct {
+	ID            string        `json:"id"`
+	Name          string        `json:"name"`
+	Description   string        `json:"description"`
+	IconClass     string        `json:"icon_class"`
+	CreatedAt     time.Time     `json:"created_at"`
+	UpdatedAt     time.Time     `json:"updated_at"`
+	Subcategories []Subcategory `json:"subcategories"` // One-to-many relationship
+	Inventories   []Inventory   `json:"inventories"`   // One-to-many relationship
+}
+
+type Subcategory struct {
+	ID          string      `json:"id"`
+	CategoryId  string      `json:"category_id"`
+	Name        string      `json:"name"`
+	Description string      `json:"description"`
+	IconClass   string      `json:"icon_class"`
+	CreatedAt   time.Time   `json:"created_at"`
+	UpdatedAt   time.Time   `json:"updated_at"`
+	Inventories []Inventory `json:"inventories"` // One-to-many relationship
+}
+
+type Inventory struct {
+	ID            string           `json:"id"`
+	Name          string           `json:"name"`
+	Description   string           `json:"description"`
+	UserId        string           `json:"user_id"`
+	CategoryId    string           `json:"category_id"`
+	SubcategoryId string           `json:"subcategory_id"`
+	Promoted      bool             `json:"promoted"`
+	Deactivated   bool             `json:"deactivated"`
+	CreatedAt     time.Time        `json:"created_at"`
+	UpdatedAt     time.Time        `json:"updated_at"`
+	Images        []InventoryImage `json:"images"` // One-to-many relationship
+}
+
+type InventoryImage struct {
+	ID          string    `json:"id"`
+	LiveUrl     string    `json:"live_url"`
+	LocalUrl    string    `json:"local_url"`
+	InventoryId string    `json:"inventory_id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }

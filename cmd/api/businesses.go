9c425f3ac833b5a7f3eab220b115f6e127104649
obynonwane <ojohnson@polygon.technology>
2025-07-12 16:52:03 +0100

    update

--- cmd/api/businesses.go
+++ cmd/api/businesses.go
@@ -2,18 +2,32 @@ package main
 
 import (
 	"context"
+	"log"
 	"net/http"
 	"time"
+
+	"github.com/obynonwane/inventory-service/data"
 )
 
 func (app *Config) PremiumPartner(w http.ResponseWriter, r *http.Request) {
+
+	var requestPayload data.SearchPremiumPartnerPayload
+
+	//2. extract the requestbody
+	err := app.readJSON(w, r, &requestPayload)
+	if err != nil {
+		app.errorJSON(w, err, nil)
+		return
+	}
+
+	log.Println("DETAILS", requestPayload)
 
 	// Create a context with a timeout for the asynchronous task
 	ctx := r.Context()
 	timeoutCtx, cancel := context.WithTimeout(ctx, 10*time.Second) // Example timeout duration
 	defer cancel()
 
-	data, err := app.Repo.GetPremiumPartners(timeoutCtx)
+	data, err := app.Repo.GetPremiumPartners(timeoutCtx, requestPayload)
 	if err != nil {
 		app.errorJSON(w, err, nil, http.StatusInternalServerError)
 		return
